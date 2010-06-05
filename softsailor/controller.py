79f88e6385e998b5059995bf5b5684c7f7039221
Jaap Versteegh <jaapie@touchi.(none)>
2010-06-05 18:26:35 +0200

    - Created boat controller

--- softsailor/controller.py
+++ softsailor/controller.py
@@ -1,4 +1,8 @@
 from boat import Boat
+from utils import *
+from conditions import Condition
+from situation import Situation
+from motion import Motion
 
 class Controller(object):
     def __init__(self, *args, **kwargs):
@@ -7,7 +11,24 @@ class Controller(object):
     def steer_heading(self, heading):
         pass
 
-    def steer_windangle(self, windangle):
+    def steer_wind_angle(self, wind_angle):
         pass
 
+
+
+
+class BoatController(Controller):
+    def __init__(self, *args, **kwargs):
+        super(BoatController, self).__init__(*args, **kwargs)
+        if len(args) > 0:
+            self.boat = args[0]
+        else:
+            self.boat = kwargs['boat']
+
+    def steer_heading(self, heading):
+        self.boat.situation.heading = normalize_angle_2pi(heading)
+
+    def steer_wind_angle(self, wind_angle):
+        heading = self.boat.condition.wind[0] - wind_angle 
+        self.steer_heading(heading)
 
