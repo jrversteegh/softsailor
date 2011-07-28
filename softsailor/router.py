985782667157095961dda3d00d89137585720ca6
Jaap Versteegh <j.r.versteegh@gmail.com>
2011-07-28 16:19:50 +0200

    - Added router and simulator objects

---
+++ softsailor/router.py
@@ -0,0 +1,13 @@
+"""
+Router module
+
+Contains an object that produces routes
+"""
+__author__ = "J.R. Versteegh"
+__copyright__ = "Copyright 2011, J.R. Versteegh"
+__contact__ = "j.r.versteegh@gmail.com"
+__version__ = "0.1"
+__license__ = "GPLv3, No Warranty. See 'LICENSE'"
+
+class Router(object):
+    pass
---
+++ softsailor/simulator.py
@@ -0,0 +1,14 @@
+"""
+Simulator module
+
+Contains an object that simulates sailing a route with a specified boat and
+conditions and returns stats on the sailed track
+"""
+__author__ = "J.R. Versteegh"
+__copyright__ = "Copyright 2011, J.R. Versteegh"
+__contact__ = "j.r.versteegh@gmail.com"
+__version__ = "0.1"
+__license__ = "GPLv3, No Warranty. See 'LICENSE'"
+
+class Simulator(object):
+    pass
