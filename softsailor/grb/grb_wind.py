6dfe378b4f084974275219d5ffa76191eed61877
Jaap Versteegh <j.r.versteegh@gmail.com>
2013-01-12 02:34:13 +0100

    - First version of grib wind that should do something -> test!

--- softsailor/grb/grb_wind.py
+++ softsailor/grb/grb_wind.py
@@ -50,11 +50,71 @@ class GribWind(InterpolledWind):
         for msg in grb:
             fcst = timedelta(hours=msg.unitOfTimeRange * msg.P2)
             t = msg.analDate + fcst
-            # Only handle u and v messages
-            if msg.paramId == 165 or msg.paramId == 166:
+            try:
+                d = data[t]
+            except KeyError:
+                d = {'fcst': fcst}
+                data[t] = d
+                # Handle u and v messages
+                if msg.paramId in [131, 165]:
+                    self._add_data(d, 'u', fcst)
+                elif msg.paramId in [132, 166]:
+                    self._add_data(d, 'v', fcst)
+                else:
+                    pass
+        self._arrays_from_data(data)
+
+    def _data_shape(self, data):
+        las, los, ts = (0, 0, 0)
+        for k, v in data.iteritems():
+            ts += 1
+            s = v['grid'][0].shape
+            if s[0] > las:
+                las = s[0]
+            if s[1] > los:
+                los = s[1]
+        return (las, los, ts)
+
+
+    def _add_data(self, dct, key, fcst):
+        try:
+            vals = dct[key]
+            # The values already exist. Only get new once in
+            # case the forecast offset is less i.e. the data is newer
+            if fcts > dct['fcst']:
+                return
+        except KeyError:
+            pass # Expected
+        dct['grid'] = np.array(msg.latlons())
+        dct[key] = np.array(msg.values)
+       
+    def _arrays_from_data(self, data):
+        # Setup grid shape and init value arrays
+        shape = self._data_shape(data)
+        self.grid = np.zeros((3,) + shape)
+        self.init_value_arrays()
+
+        times = sorted(data.keys())
+        if len(times) == 0:
+            return
+        self.start = times[0]
+        secs = np.zeros(len(times))
+        # Loop over the data
+        for i, t in enumerate(times):
+            secs[i] = (t - self.start).total_seconds()
+            d = data[t]
+            self.u[:,:,i] = d['u']
+            self.v[:,:,i] = d['v']
+            self.grid[:2,:,:,i] = d['grid']
+
+        # Set time coordinates of the mesh
+        self.grid[2,:,:] = secs
+            
 
 
 
+
+        
 
 
 
